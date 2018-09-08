ea5e76d31f29f1933e094b1ac723ae23bcf24950
dmknght <dmknght@localhost.localdomain>
2018-09-08 08:15:44 +0700

    Remove space

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ except ImportError as ImportError:
 ########################## SSL
 #	https://stackoverflow.com/a/35960702
 try:
-    _create_unverified_https_context = ssl._create_unverified_context
+	_create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
-    pass
+	# Legacy Python that doesn't verify HTTPS certificates by default
+	pass
 else:
-    # Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
+	# Handle target environment that doesn't support HTTPS verification
+	ssl._create_default_https_context = _create_unverified_https_context
 ########################## End ssl
 
 def main(setTargetURL, setOptions, setMode, setRunOptions):
