4bc39a62f016fb3bf9df5b5bdf8af10ab1c68a1b
dmknght <dmknghtx2team@gmail.com>
2018-09-28 23:51:36 +0700

    Update import err

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ def main(optionURL, setOptions, optionRunMode, setRunOptions):
 	try:
 		import mechanize, re, ssl, requests # for basichttpauthentication, not useless, use later
 	except ImportError as err:
-		print(err)
 		#_, missing_moudle, _ = str(err).split("'")
 		#sys.exit("Try: sudo apt install python-%s" %(missing_moudle))
+		utils.die("Import module error", err)
 			
 	try:
 		_create_unverified_https_context = ssl._create_unverified_context
