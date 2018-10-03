a303be5668da3c838a8f53f876db6e1f4bf6d28a
dmknght <dmknghtx2team@gmail.com>
2018-10-03 08:22:02 +0700

    improve some logic in form parsing

--- core/tbrowser.py
+++ core/tbrowser.py
@@ -11,7 +11,31 @@ def startBrowser():
 	browser.set_handle_refresh(True) 
 	#browser._factory.is_html = True #https://stackoverflow.com/a/4201003
 	return browser
-	
+
+def parsePasswdForm(objBrowserForm):
+	##########################################
+	#	Get Password-only form
+	#	Need form ID for select_form(nr = ID)
+	#	Password field's name for submiting
+	#
+	###########################################
+
+	# Using for loop for getting
+	retFormID = 0
+
+	regPassField = r"PasswordControl\W(.*)="
+
+	# Find login form
+	for form in objBrowserForm:
+		try:
+			#retTextField = re.findall(regTextField, str(form).encode('utf-8'), re.MULTILINE)[0]
+			#retPassField = re.findall(regPassField, str(form).encode('utf-8'), re.MULTILINE)[0]
+			retPassField = re.findall(regPassField, str(form), re.MULTILINE)[0]
+			return (retFormID, [retPassField, retTextField])
+		except:
+			retFormID += 1
+	return None
+
 def parseLoginForm(objBrowserForm):
 	##########################################
 	#	Get Login Form Information
@@ -30,13 +54,12 @@ def parseLoginForm(objBrowserForm):
 	regPassField = r"PasswordControl\W(.*)="
 
 	# Find login form
-	for idxSingleForm in objBrowserForm:
+	for form in objBrowserForm:
 		try:
-			#retTextField = re.findall(regTextField, str(idxSingleForm).encode('utf-8'), re.MULTILINE)[0]
-			#retPassField = re.findall(regPassField, str(idxSingleForm).encode('utf-8'), re.MULTILINE)[0]
-			retTextField = re.findall(regTextField, str(idxSingleForm), re.MULTILINE)[0]
-			retPassField = re.findall(regPassField, str(idxSingleForm), re.MULTILINE)[0]
-			#return retFormID, retTextField, retPassField
+			#retTextField = re.findall(regTextField, str(form).encode('utf-8'), re.MULTILINE)[0]
+			#retPassField = re.findall(regPassField, str(form).encode('utf-8'), re.MULTILINE)[0]
+			retTextField = re.findall(regTextField, str(form), re.MULTILINE)[0]
+			retPassField = re.findall(regPassField, str(form), re.MULTILINE)[0]
 			return (retFormID, [retPassField, retTextField])
 		except:
 			retFormID += 1
@@ -64,8 +87,8 @@ def getLoginForm(optionURL, browser, verbose):
 			utils.printf("[*] Found login form", "good")
 		return formInfo
 
-	except TypeError as error:
-		utils.die("getLoginForm: Can not find login form", error)
+	# except TypeError as error:
+	# 	utils.die("getLoginForm: Can not find login form", error)
 
 	except Exception as error:
 		utils.die("getLoginForm: Runtime error", error)
