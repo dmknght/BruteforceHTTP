16c3515ce186c90c1a580543c6f916bf372c97b7
dmknght <dmknghtx2team@gmail.com>
2018-10-13 23:54:55 +0700

    Print table with separator lines

--- core/utils.py
+++ core/utils.py
@@ -64,38 +64,64 @@ def print_table(headers, *args, **kwargs):
 	extra_fill = kwargs.get("extra_fill", 5)
 	header_separator = kwargs.get("header_separator", "-")
 	if not all(map(lambda x: len(x) == len(headers), args)):
-		printf("Error headers", 'bad')
-		return
+		printf("[x] PrintTable: Error headers", 'bad')
+		return None
+
 	def custom_len(x):
 		try:
 			return len(x)
 		except TypeError:
 			return 0
+
+	##### CRAFTING HEADER ######
 	fill = []
-	headers_line = '   '
-	headers_separator_line = '   '
+
+	# headers_line += label: Filling_header
+	# headers_line = headers_line + "Lable 1 | Label 2"
+	headers_line = '  | '
+	headers_separator_line = '  +'
 
 	for idx, header in enumerate(headers):
 		column = [custom_len(arg[idx]) for arg in args]
 		column.append(len(header))
 		current_line_fill = max(column) + extra_fill
 		fill.append(current_line_fill)
-		headers_line = "".join((headers_line, "{header:<{fill}}".format(header = header, fill = current_line_fill)))
-		headers_separator_line = "".join((
-			headers_separator_line,
-			'{:<{}}'.format(header_separator * len(header), current_line_fill)
-		))
-	print(headers_line)
-	print(headers_separator_line)
+		# label: Filling_header
+		headers_line = "%s%s" %(
+			"".join((headers_line, "{header:<{fill}}".format(header = header, fill = current_line_fill))),
+			"| "
+			)
+
+		headers_separator_line = "%s%s" %(
+			"-".join((
+				headers_separator_line,
+				'{:<{}}'.format(header_separator * current_line_fill, current_line_fill)
+			)),
+			"+"	
+		)
+		
+	# End of crafting header
+
+	# Print header
+	print("%s\n%s\n%s" %(headers_separator_line, headers_line, headers_separator_line))
+
+
+	# Print contents
 	for arg in args:
-		content_line = '   '
+		content_line = '  | ' # print first character before contents
 		for idx, element in enumerate(arg):
-			content_line = "".join((
-				content_line,
-				'{:{}}'.format(element, fill[idx])
-			))
+			content_line = "%s%s" %(
+				"".join((
+					content_line,
+					'{:{}}'.format(element, fill[idx])
+				)),
+				"| "	
+			)
 		print(content_line)
 		
+	# Print end line
+	print(headers_separator_line)
+
 def print_help():
 
 	#	Print project's help table
