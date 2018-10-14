a52e8d669dd5358f34a5cf32898e22154a279931
dmknght <dmknghtx2team@gmail.com>
2018-10-14 15:55:31 +0700

    Import mechanize 3.5

--- core/tbrowser.py
+++ core/tbrowser.py
@@ -4,8 +4,8 @@ from core import actions, utils
 def startBrowser():
 	import mechanize
 	#https://stackoverflow.com/a/27096416
-	browser = mechanize.Browser(factory=mechanize.RobustFactory())
-	#browser = mechanize.Browser()
+	#browser = mechanize.Browser(factory=mechanize.RobustFactory())
+	browser = mechanize.Browser()
 	browser.set_handle_robots(False)
 	browser.set_handle_referer(True)
 	browser.set_handle_redirect(True)
