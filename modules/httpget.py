b3d40b962786c03344ab6dee674b8cc65466d130
dmknght <dmknght@localhost.localdomain>
2019-04-21 01:59:46 +0700

    Fix import lib err and shorter login logic

--- modules/httpget.py
+++ modules/httpget.py
@@ -1,5 +1,6 @@
 from libs.mbrowser import mBrowser
 from utils.utils import printf, die
+from cores.actions import randomFromList
 
 # https://stackoverflow.com/a/4089075
 
@@ -16,29 +17,26 @@ def submit(options, loginInfo, creds, result):
 			proxyAddr = randomFromList(options.proxy)
 			proc.setproxy({"http": proxyAddr})
 		proc.httpget_passwd(options.url, tryUsername, tryPassword, realm)
+		
 		if options.verbose:
 			if options.proxy:
 				printf("[+] {%s: %s; %s: %s through %s}" %(loginInfo[1][1], tryUsername, loginInfo[1][0], tryPassword, proxyAddr), 'norm')
 			else:
 				printf("[+] {%s: %s; %s: %s}" %(loginInfo[1][1], tryUsername, loginInfo[1][0], tryPassword), 'norm')
+
 		proc.open_url(options.url)
+		printf("[*] %s [%s]" %([tryUsername, tryPassword], proc.title()), "good")
+		result.put([options.url, tryUsername, tryPassword])
+
+	except Exception as err:
 		try:
-			# Re open URL. If HTTP.code = 200 == success
-			proc.open_url(options.url)
-			printf("[*] %s [%s]" %([tryUsername, tryPassword], proc.title()), "good")
-			result.put([options.url, tryUsername, tryPassword])
-		except Exception as err:
-			try:
-				if type(err.code) == int and err.code == 401:
-					if options.verbose:
-						if options.proxy:
-							printf("[-] Failed: %s through %s" %([tryUsername, tryPassword], proxyAddr), "bad")
-						else:
-							printf("[-] Failed: %s" %([tryUsername, tryPassword]), "bad")
-				else:
-					printf("[x] %s: %s" %(err, creds[::-1]), "bad")
-			except:
-				die("[x] HTTP GET:", err)
-	except Exception as error:
-		if options.verbose:
-			printf("[x] Failed!", "bad")
+			if type(err.code) == int and err.code == 401:
+				if options.verbose:
+					if options.proxy:
+						printf("[-] Failed: %s through %s" %([tryUsername, tryPassword], proxyAddr), "bad")
+					else:
+						printf("[-] Failed: %s" %([tryUsername, tryPassword]), "bad")
+			else:
+				printf("[x] %s: %s" %(err, creds[::-1]), "bad")
+		except:
+			die("[x] HTTP GET:", err)
