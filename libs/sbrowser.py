2c92e72cb97fd0decdf2915320507893c28d2c87
dmknght <dmknght@localhost.localdomain>
2019-06-22 18:00:49 +0700

    submitable selenium

--- libs/sbrowser.py
+++ libs/sbrowser.py
@@ -34,15 +34,14 @@ class sBrowser(Chrome):
 	def get_opts(self, options):
 		self.options = options
 
-	def geturl(self):
-		pass
+	def url(self):
+		return self.current_url
 
 	def get_resp(self):
 		return self.page_source
 	
 	def get_title(self):
-		pass
-
+		return self.title
 
 	def forms(self):
 		"""Get a summary of the form with classic mechanize style
@@ -81,11 +80,12 @@ class sBrowser(Chrome):
 		# usrname, passwd = creds
 		# self.find_element_by_name(usrField).send_keys(usrname)
 		# self.find_element_by_name(passField).send_keys(passwd)
-		if button[0]:
-			self.find_element_by_name(button[0]).click()
-		else:
-			print("Can't click") # DEBUG
-			pass # https://stackoverflow.com/a/35532972
+		return self.find_element_by_name(button).click()
+		# if button[0]:
+		# 	self.find_element_by_name(button[0]).click()
+		# else:
+		# 	print("Can't click") # DEBUG
+			# pass # https://stackoverflow.com/a/35532972
 
 	def close(self):
 		# TODO add clear storage data here
@@ -94,67 +94,4 @@ class sBrowser(Chrome):
 		except:
 			pass
 
-# def startBrowser():
-# 	# 1. Download webdriver and throw it into PATH (make sure it can be executed - 755)
-# 	# 2. Install chromium
-# 	try:
-# 		options = webdriver.chrome.options.Options()
-# 		options.headless = True
-
-# 		brow = webdriver.Chrome(chrome_options=options)
-# 		# brow.get('http://giapbat.gsatgt.vn:8088/#/login')
-# 		# brow.get(url)
-
-# 		# resp = str("%s" %(brow.page_source))
-
-# 		# import mechanize
-# 		# mbrow = mechanize.Browser()
-# 		# # mresp = mbrow.open('http://giapbat.gsatgt.vn:8088/#/login')
-# 		# mresp = mbrow.open(url)
-# 		# mresp.set_data(resp)
-# 		# mbrow.set_response(mresp)
-# 		# # Parse login form here
-# 		# from cores.mbrowser import parseLoginForm
-# 		# lInfo = parseLoginForm(mbrow.forms())
-# 		# if not lInfo:
-# 		# 	print "Exit! No login form"
-# 		# else:
-# 		# 	fid, info = lInfo
-# 		# 	fid, btn = fid
-# 		# 	passField, usrField = info
-# 		# 	for form in mbrow.forms():
-# 		# 		print form
-# 		# 	brow.find_element_by_name(usrField).send_keys("admin")
-# 		# 	brow.find_element_by_name(passField).send_keys("admin")
-# 		# 	brow.find_element_by_name(btn).click()
-
-# 		# 	print brow.title
-# 			# form = brow.find_element_by_id('id')
-# 			# form.submit()
-# 			# if "Sai" in brow.page_source:
-# 			# 	print "Failed"
-# 			# else:
-# 			# 	print "Not sure"
-
-# 			# for form in mbrow.forms():
-# 			# 	print form
-# 		# Test submit with mechanize
-# 		# Done well with non java script sites
-# 			# mbrow.select_form(nr = fid)
-# 			# mbrow.form[usrField] = 'admin'
-# 			# mbrow.form[passField] = 'admin'
-# 			# mbrow.submit()
-# 			# # print mbrow.title()
-# 			# print mbrow.geturl()
-# 			# print mbrow.response().read()
-
-# 		# Test submit with selenium
-		
-# 	except Exception as error:
-# 		print error
-# 	finally:
-# 		try:
-# 			brow.quit()
-# 		except:
-# 			pass
 
