91a3c9d5846ce8942808eb281a98f5124a5b7408
dmknght <dmknght@protonmail.com>
2019-09-02 03:41:13 +0700

    Change color and format

--- utils/events.py
+++ utils/events.py
@@ -14,12 +14,15 @@ def warn(data, info = "INFO"):
 	print("[-] [\033[33m%s\033[00m] %s" % (info, data))
 
 
-def fail(data, info = "FAILED"):
-	print("[\033[31m-\033[00m] [\033[31m%s\033[00m] %s" % (info, data))
+def fail(account, msg = "", title = "", info = "FAILED"):
+	if not msg and not title:
+		print("[\033[31m-\033[00m] [\033[31m%s\033[00m] %s" % (info, account))
+	else:
+		print("[\033[31m-\033[00m] [\033[31m%s\033[00m] [\033[34m%s\033[00m] --> [\033[37m%s\033[00m] %s" % (info, msg, title, account))
 
 
 def found(user, passwd, title):
-	print("[\033[37m*\033[00m] [\033[31mMATCH\033[00m] ['\033[32m%s\033[00m':'\033[32m%s\033[00m'] [\033[32m%s\033[00m]" % (user, passwd, title))
+	print("[\033[37m*\033[00m] [\033[36mMATCH\033[00m] ['\033[32m%s\033[00m':'\033[32m%s\033[00m'] [\033[37m%s\033[00m]" % (user, passwd, title))
 	
 	
 def vuln(vuln):
